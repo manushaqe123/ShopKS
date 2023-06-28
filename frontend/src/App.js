f30a6cdd23ea5dec2001267406701208d8754072
BlertaHajdini <72973850+BlertaHajdini@users.noreply.github.com>
2023-06-28 20:38:29 +0200

    Renaming home and product screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom'
 import './App.css';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
+import HomeScreen from './screens/HomeScreen1';
+import ProductScreen from './screens/ProductScreen1';
 import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
 import { useSelector } from 'react-redux';
